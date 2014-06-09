866e0a107d3840749ce5e6329c7cfe70ef645c32
Ryan King <ryancharlesking@gmail.com>
2014-06-08 20:32:21 -0700

    getting the app going

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,34 @@
-(function (){
-    var app = angular.module('lifeCounter', ['Player']);
+﻿(function(){
+	var app = angular.module('lifeCounter', []);
+	app.controller('GameController', function(){
+		
+		var self = this;
+		
+		var initialPlayers = [{
+			'name': 'Player 1', 'life': 40, 'poison':0
+		},{
+			'name': 'Player 2', 'life': 40, 'poison':0
+		}];
+		this.players = initialPlayers.concat();
+		
+		this.addPlayer = function(){
+			self.players.push({
+				'name': 'Player ' + (self.players.length +1), 
+				'life': 40, 
+				'poison':0
+			});
+		};
+		
+		this.restart = function(){
+			self.players = initialPlayers.concat();
+		};
+		
+	});
+	
+	/*app.directive('player', function(){
+		return {
+			'restrict': 'E',
+			'templateUrl: 'scripts/playerModule/player.html'
+		};
+	});*/
 })();
--- scripts/playerModule/player.js
+++
@@ -1,5 +0,0 @@
-(function (){
-    var app = angular.module('Player', []);
-
-    //app.controller('')
-})();
