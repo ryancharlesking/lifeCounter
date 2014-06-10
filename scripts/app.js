2ae2fab1b608e1c4ba2e88591b74481f8e5f66e6
Ryan King <ryancharlesking@gmail.com>
2014-06-09 19:21:51 -0700

    Added general damage counter and styles

--- scripts/app.js
+++ scripts/app.js
@@ -7,11 +7,26 @@
 		this.players = [];
 		
 		this.addPlayer = function(){
-			self.players.push({
+			var newPlayer = {
 				'name': 'Player ' + (self.players.length +1), 
 				'life': 40, 
-				'poison':0
-			});
+				'poison':0,
+				'generalDamage': []
+			}
+			for(var i=0, len = self.players.length; i<len; i++){
+				newPlayer.generalDamage.push({
+					'player': self.players[i],
+					'damage': 0
+				});
+			}
+			self.players.push(newPlayer);
+			for(var i=0, len = self.players.length-1; i<len; i++){
+				self.players[i].generalDamage.push({
+					'player': newPlayer,
+					'damage': 0
+				});
+			}
+			
 		};
 		
 		this.updatePlayerLife = function(amount, player){
@@ -22,12 +37,20 @@
 			player.poison += amount;
 		};
 		
+		this.setPlayerRenaming = function($event, player, val){
+			player.renaming = val;
+			if(val){
+				setTimeout(function(){$event.target.parentNode.getElementsByTagName('input')[0].select();}, 0);
+			}
+			if(!player.name){
+				player.name = 'Player';
+			}
+		};
+		
 		this.restart = function(){
-			self.players = [{
-				'name': 'Player 1', 'life': 40, 'poison':0
-			},{
-				'name': 'Player 2', 'life': 40, 'poison':0
-			}];
+			self.players = [];
+			self.addPlayer();
+			self.addPlayer();
 		};
 		
 		self.restart();
